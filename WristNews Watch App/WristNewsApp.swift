c23317de3a15c33f800dbc508d0dfa4da0b711b4
Jeff Pham <larrypham.vn@gmail.com>
2023-06-25 23:32:18 +0700

    Initial Commit

---
+++ WristNews Watch App/WristNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristNewsApp.swift
+//  WristNews Watch App
+//
+//  Created by Phi Anh on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WristNews_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
